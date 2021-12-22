f8d7e0ab7be6d8452d4b0f7a0fe449f169a27f58
NobodyExpectsTheSpanishInquisition <s.miklaszewski1999@gmail.com>
2021-12-22 20:59:29 +0100

    feat(docker): configure php

---
+++ Dockerfile.php
@@ -0,0 +1,9 @@
+ARG PHP_VERSION
+FROM php:$PHP_VERSION
+
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer creates=/usr/local/bin/composer warn=no
+
+RUN pecl install xdebug
+
+ARG PATH
+WORKDIR $PATH
